51ee8356fab20adefac24d1c603acbd12d61b264
satheesh2705 <mrsatheesh2705@gmail.com>
2024-06-24 12:38:29 +0530

    24-jun-24

--- src/Componants/PremiumPrizeList/BasicPrize.jsx
+++ src/Componants/PremiumPrizeList/BasicPrize.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import './summa.css';
 
 const BasicPrize = () => {
   return (
-    <div className='BasicPrize'>
-       
+    <div className='BasicPrize' >
+       <div className='PrizeBox'>
+       <p>Basic</p>
+       <div className='PremiumAmount'>
+          <h1>243.35</h1>
+          <p>/ month</p>
+       </div>
+       <div className='PremiumButton'>
+          <button>Subscribe</button>
+       </div>
+       <div className='PremiumFeatures'>
+         <ul>
+            <li> <span>&#10003;</span> <p> Small reply boost </p> </li>
+            <li> <span>&#10003;</span> <p> Encrypted direct messages </p> </li>
+            <li> <span>&#10003;</span>  <p> Bookmark folders </p></li>
+            <li> <span>&#10003;</span> <p> Highlights tab </p> </li>
+            <li> <span>&#10003;</span> <p> Edit post </p> </li>
+            <li> <span>&#10003;</span> <p> Post longer videos </p></li>
+            <li>  <span>&#10003;</span> <p> Longer posts </p></li>
+             
+          
+           
+          
+          
+         </ul>
+       </div>
+       </div>
     </div>
   )
 }
